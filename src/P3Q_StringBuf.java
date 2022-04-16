eaff0cc112c9667eae529ead95f822e48adcadf7
DELL <rizkiakmal37@gmail.com>
2022-04-16 12:17:29 +0700

    Menggunakan kelas StringBuffer

---
+++ src/P3Q_StringBuf.java
@@ -0,0 +1,11 @@
+public class P3Q_StringBuf {
+    public static void main(String[] args) {
+        String str = "Java";
+        StringBuffer buffer = new StringBuffer(str);
+        if (str.equals(buffer)){
+            System.out.println("Both are equal");
+        }else {
+            System.out.println("Both are not equal");
+        }
+    }
+}
