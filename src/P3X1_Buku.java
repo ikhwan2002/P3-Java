dd54207ded30904560edcda6b97eb4137f50ef4b
DELL <rizkiakmal37@gmail.com>
2022-04-18 14:19:07 +0700

    Mengisi value HashMap dengan objek Buku

---
+++ src/P3X1_Buku.java
@@ -0,0 +1,26 @@
+public class P3X1_Buku {
+
+    private String title;
+    private String author;
+
+    public P3X1_Buku(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
