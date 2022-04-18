2d680efe7732313732cb82b14d54ba9dbebd4a76
DELL <rizkiakmal37@gmail.com>
2022-04-18 13:44:14 +0700

    Nengurutkan elemen ArrayList menggunakan kelas Collection

---
+++ src/P3U_ArrayListSorted.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class P3U_ArrayListSorted {
+    public static void main(String[] args) {
+        ArrayList<Integer> myNumbers = new ArrayList<>();
+        myNumbers.add(20);
+        myNumbers.add(15);
+        myNumbers.add(25);
+        myNumbers.add(10);
+        printList(myNumbers);
+
+        Collections.sort(myNumbers);
+        printList(myNumbers);
+
+        Collections.sort(myNumbers, Collections.reverseOrder());
+        printList(myNumbers);
+    }
+
+    static void printList(ArrayList<Integer>myNumbers){
+        myNumbers.forEach((number)->{
+            System.out.printf("%d ", number);
+        });
+        System.out.println();
+    }
+}
